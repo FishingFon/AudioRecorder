d05f2bfe2638802ba867d9d699635451f1acb44f
Corey Bacon <fishingfon@gmail.com>
2014-11-07 21:48:45 +1000

    Version 1.0

--- app/src/main/java/com/bacon/corey/audiotimeshift/CustomAdapter.java
+++ app/src/main/java/com/bacon/corey/audiotimeshift/CustomAdapter.java
@@ -29,7 +29,6 @@ public class CustomAdapter extends ArrayAdapter {
         TextView title = (TextView) rowView.findViewById(R.id.title);
         TextView date = (TextView) rowView.findViewById(R.id.date);
         TextView length = (TextView) rowView.findViewById(R.id.length);
-        
 
         return super.getView(position, convertView, parent);
     }
