edcc543eab0b7bc950a80f347350eef99aafc144
Corey Bacon <fishingfon@gmail.com>
2014-11-08 01:05:08 +1000

    Version 1.1

--- app/src/main/java/com/bacon/corey/audiotimeshift/MainActivity.java
+++ app/src/main/java/com/bacon/corey/audiotimeshift/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,15 +17,21 @@ import android.widget.Toast;
 
 
 public class MainActivity extends FragmentActivity {
+    static final int ITEMS = 3;
+    ViewPagerAdapter vAdapter;
+    ViewPager vPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();}
+
+        vAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        vPager = (ViewPager) findViewById(R.id.viewPager);
+        vPager.setAdapter(vAdapter);
+
+
+
         }
 
 
@@ -44,7 +53,6 @@ public class MainActivity extends FragmentActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
     public static class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
@@ -53,14 +61,11 @@ public class MainActivity extends FragmentActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_test, container, false);
 
             return rootView;
         }
     }
-
-
-
     public void play(View view){
         Toast.makeText(this, "play", Toast.LENGTH_SHORT).show();
 
@@ -70,8 +75,6 @@ public class MainActivity extends FragmentActivity {
 
 
     }
-
-
     public void record(View view){
         Toast.makeText(this, "record", Toast.LENGTH_SHORT).show();
 
@@ -86,7 +89,32 @@ public class MainActivity extends FragmentActivity {
         stopService(intent);
     }
 
+    public static class ViewPagerAdapter extends FragmentPagerAdapter {
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
+        @Override
+        public int getCount() {
+            return ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return TestFragment.newInstance(position);
+                case 1:
+                    return FilesListFragment.newInstance(position);
+                case 2:
+                    return RecordOptionsFragment.newInstance(position);
+
+                default:
+                    return TestFragment.newInstance(position);
+            }
+
+        }
+    }
 
 
 
